e2ebba6a0697bc09b2de777e5cf3ffbc8a16e9a9
taekwombo <88032359+taekwombo@users.noreply.github.com>
2021-10-14 21:08:10 +0200

    chore: comment out unused code

--- keyboards/lily58/keymaps/taekwombo/rules.mk
+++ keyboards/lily58/keymaps/taekwombo/rules.mk
@@ -18,10 +18,10 @@ OLED_ENABLE= yes     		# OLED display
 SLEEP_LED_ENABLE = no    	# Breathing sleep LED during USB suspend
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
+SRC +=  ./lib/keylogger.c \
         ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
+		# ./lib/rgb_state_reader.c \
+        # ./lib/timelogger.c \
+        # ./lib/logo_reader.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -5,9 +5,6 @@
 
 #define L_BASE 0
 #define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 
 char layer_state_str[24];
 
@@ -17,15 +14,8 @@ const char *read_layer_state(void) {
   case L_BASE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
   case L_LOWER:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
