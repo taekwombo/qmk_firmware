cc48c98446831404dbb151d640d7f28bf27623fe
taekwombo <88032359+taekwombo@users.noreply.github.com>
2022-07-09 14:31:32 +0200

    chore(taekwombo): add another layer

--- keyboards/lily58/keymaps/taekwombo/keymap.c
+++ keyboards/lily58/keymaps/taekwombo/keymap.c
@@ -3,6 +3,7 @@
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
+  _RAISE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -27,30 +28,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,   \
   KC_BSLS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
-                             KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,  KC_ENT,   KC_RCTL, MO(_LOWER), KC_RALT \
+                             KC_LALT, KC_LGUI, KC_LCTL, KC_SPC,  KC_ENT,   KC_RCTL, MO(_LOWER), KC_RALT                           \
 ),
 
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  | F3   | F4   |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      | VOLU | BRIU |      |                    | PSCR | INS  | HOME | PGUP | PAUS | SLCK |
+ * |      |      |      |      |      |      |                    | CAPS | INS  | DEL  | APP  | HOME | PGUP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      | VOLD | BRID |      |-------.    ,-------| BSPC | DEL  | END  | PGDN | APP  | CAPS |
- * |------+------+------+------+------+------|  ESC  |    |       |------+------+------+------+------+------|
- * |      |      |      | Mute |      |      |-------|    |-------| LEFT | DOWN |  UP  | RGHT |      |      |
+ * |      |      |  (   |  [   |  {   | ESC  |-------.    ,-------| BSPC |  }   |  ]   |  )   | END  | PGDN |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------| LEFT | DOWN |  UP  | RGHT |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |RESET |      |      | /       /       \      \  |      |      |      |
+ *                   |      |RAISE |      | /       /       \      \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_LOWER] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   \
-  KC_NO,   KC_NO,   KC_NO,   KC_VOLU, KC_BRIU, KC_NO,                     KC_PSCR, KC_INS,  KC_HOME, KC_PGUP, KC_PAUS, KC_SLCK,  \
-  KC_NO,   KC_NO,   KC_NO,   KC_VOLD, KC_BRID, KC_NO,                     KC_BSPC, KC_DEL,  KC_END,  KC_PGDN, KC_APP,  KC_CAPS,  \
-  KC_NO,   KC_NO,   KC_NO,   KC_MUTE, KC_NO,   KC_NO, KC_ESC,    KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,    \
-                             RESET,   KC_NO,   KC_NO, KC_NO,     KC_NO,   KC_NO, KC_NO, KC_NO \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   \
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                    KC_CAPS, KC_INS,  KC_DEL,  KC_APP,  KC_HOME, KC_PGUP,  \
+  KC_NO,   KC_NO,   KC_LPRN, KC_LBRC, KC_LCBR,   KC_NO,                    KC_BSPC, KC_RCBR, KC_RBRC, KC_RPRN, KC_END,  KC_PGDN,  \
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_ESC, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,    \
+                             KC_NO,   MO(_RAISE),KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO                                \
+),
+
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | RESET|      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | BRIU |      | MNXT | VOLU | MPLY |                    |      | ACL0 | ACL1 | ACL2 |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      | BRID |      | MPRV | VOLD | MSTP |-------.    ,-------|      | BTN1 | BTN3 | BTN3 |      |      |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |      |      |      |      | MUTE |      |-------|    |-------| MS_L | MS_D | MS_U | MS_R |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /       /       \      \  |      |      |      |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+
+[_RAISE] = LAYOUT( \
+  RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,                    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   \
+  KC_NO,   KC_BRIU, KC_NO,   KC_MNXT, KC_VOLU,   KC_MPLY,                  KC_NO,   KC_ACL0, KC_ACL1, KC_ACL2, KC_NO,   KC_NO,   \
+  KC_NO,   KC_BRID, KC_NO,   KC_MPRV, KC_VOLD,   KC_MSTP,                  KC_NO,   KC_BTN1, KC_BTN2, KC_BTN3, KC_NO,   KC_NO,   \
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MUTE,   KC_NO,  KC_NO,   KC_NO,   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO,   KC_NO,   \
+                             KC_NO,   KC_NO,     KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO                               \
 )
 
 };
@@ -62,11 +86,12 @@ const char *read_layer_state(void);
 void set_keylog(keyrecord_t *record);
 const char *read_keylog(void);
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   if (is_keyboard_master()) {
     oled_write_ln(read_layer_state(), false);
     oled_write_ln(read_keylog(), false);
   }
+  return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
