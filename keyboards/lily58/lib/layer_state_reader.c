d0b2d5b73ecd4efcdd2fba1646cb5de841bb07b2
taekwombo <88032359+taekwombo@users.noreply.github.com>
2022-07-09 14:31:32 +0200

    chore(taekwombo): add another layer

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,24 +1,25 @@
-
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include "lily58.h"
 
-#define L_BASE 0
-#define L_LOWER (1 << 1)
-
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+    switch (get_highest_layer(layer_state)) {
+      case 0:
+        snprintf(layer_state_str, sizeof(layer_state_str), "L: Qwerty");
+        break;
+
+      case 1:
+        snprintf(layer_state_str, sizeof(layer_state_str), "L: Lower");
+        break;
+
+      case 2:
+        snprintf(layer_state_str, sizeof(layer_state_str), "L: Raise");
+        break;
+
+      default:
+        snprintf(layer_state_str, sizeof(layer_state_str), "L: Undef-%ld", layer_state);
   }
 
   return layer_state_str;
